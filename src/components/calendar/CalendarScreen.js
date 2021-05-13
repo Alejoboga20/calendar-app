942bea690d366ce88c3bd2b15dc6a8e70d8e8f2b
alejoboga <alejoboga20@gmail.com>
2021-05-13 10:15:49 -0300

    Setup react big calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: 'Happy Birthday Boss',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+  }
+];
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className='calendar-screen'>
       <Navbar />
+
+      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' />
     </div>
   );
 };
