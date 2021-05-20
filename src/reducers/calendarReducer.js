036876c0519f3c74d3807710823677abfed23c03
Alejo boga <alejoboga20@gmail.com>
2021-05-20 09:40:31 -0300

    Adding case in reducer

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 const initialState = {
   events: [
     {
+      id: new Date().getDate(),
       title: 'Happy Birthday Boss',
       start: moment().toDate(),
       end: moment().add(2, 'hours').toDate(),
@@ -34,6 +35,11 @@ export const calendarReducer = (state = initialState, action) => {
       return {
         ...state,
         activeEvent: null
+      };
+    case types.eventUpdated:
+      return {
+        ...state,
+        events: state.events.map((e) => (e.id === action.payload.id ? action.payload : e))
       };
     default:
       return state;
