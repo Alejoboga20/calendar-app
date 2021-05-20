15a98cdfd9fa4c9b1c90bbe4102ca9e56d598a49
Alejo boga <alejoboga20@gmail.com>
2021-05-20 09:43:15 -0300

    Dispatching update evenet

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import Swal from 'sweetalert2';
 import { useDispatch, useSelector } from 'react-redux';
 import { uiCloseModal } from '../../actions/ui';
-import { eventAddNew, eventClearActiveEvent } from '../../actions/events';
+import { eventAddNew, eventClearActiveEvent, eventUpdated } from '../../actions/events';
 
 const customStyles = {
   content: {
@@ -80,16 +80,20 @@ export const CalendarModal = () => {
       return setTitleValid(false);
     }
 
-    dispatch(
-      eventAddNew({
-        ...formValues,
-        id: new Date().getTime(),
-        user: {
-          _id: '123',
-          name: 'Alejo'
-        }
-      })
-    );
+    if (activeEvent) {
+      dispatch(eventUpdated(formValues));
+    } else {
+      dispatch(
+        eventAddNew({
+          ...formValues,
+          id: new Date().getTime(),
+          user: {
+            _id: '123',
+            name: 'Alejo'
+          }
+        })
+      );
+    }
 
     setTitleValid(true);
     closeModal();
