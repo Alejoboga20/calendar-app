28ea5f4ee79248035a00255ae8cb5d8630c4d600
Alejo Boga <alejoboga20@gmail.com>
2021-06-04 10:02:24 -0300

    Creating login form

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import useForm from '../../hooks/useForm';
 import './login.css';
 
+const initialLoginState = {
+  lEmail: '',
+  lPassword: ''
+};
+
 export const LoginScreen = () => {
+  const [formLoginValues, handleLoginInputChange] = useForm(initialLoginState);
+
+  const { lEmail, lPassword } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log(formLoginValues);
+  };
+
   return (
     <div className='container login-container'>
       <div className='row'>
         <div className='col-md-6 login-form-1'>
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className='form-group'>
-              <input type='text' className='form-control' placeholder='Correo' />
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Correo'
+                name='lEmail'
+                value={lEmail}
+                onChange={handleLoginInputChange}
+              />
             </div>
             <div className='form-group'>
-              <input type='password' className='form-control' placeholder='Contraseña' />
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Contraseña'
+                name='lPassword'
+                value={lPassword}
+                onChange={handleLoginInputChange}
+              />
             </div>
             <div className='form-group'>
               <input type='submit' className='btnSubmit' value='Login' />
@@ -24,17 +54,33 @@ export const LoginScreen = () => {
           <h3>Registro</h3>
           <form>
             <div className='form-group'>
-              <input type='text' className='form-control' placeholder='Nombre' />
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Nombre'
+              />
             </div>
             <div className='form-group'>
-              <input type='email' className='form-control' placeholder='Correo' />
+              <input
+                type='email'
+                className='form-control'
+                placeholder='Correo'
+              />
             </div>
             <div className='form-group'>
-              <input type='password' className='form-control' placeholder='Contraseña' />
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Contraseña'
+              />
             </div>
 
             <div className='form-group'>
-              <input type='password' className='form-control' placeholder='Repita la contraseña' />
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Repita la contraseña'
+              />
             </div>
 
             <div className='form-group'>
