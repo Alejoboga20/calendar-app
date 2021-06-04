82a9626bc7f4c3a97cc8120b343b2e411d6b3344
Alejo Boga <alejoboga20@gmail.com>
2021-06-04 09:52:31 -0300

    Adding types for auth

--- src/types/types.js
+++ src/types/types.js
@@ -6,5 +6,13 @@ export const types = {
   eventSetActive: '[event] Set Active',
   eventClearActiveEvent: '[event] Clear Active event',
   eventUpdated: '[event] Event update',
-  eventDeleted: '[event] Event deleted'
+  eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start Register',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout'
 };
