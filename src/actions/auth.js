551b31116b0c034f28a2e269bb6f7ad746d746a6
Alejo Boga <alejoboga20@gmail.com>
2021-06-05 12:00:41 -0300

    Action for register

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { fetchWithoutToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
@@ -18,6 +19,31 @@ export const startLogin = (email, password) => {
       const { uid, name } = body;
 
       dispatch(login({ uid, name }));
+    } else {
+      Swal.fire('Error', body.msg, 'error');
+    }
+  };
+};
+
+export const startRegister = (name, email, password) => {
+  return async (dispatch) => {
+    const response = await fetchWithoutToken(
+      'auth/new',
+      { name, email, password },
+      'POST'
+    );
+
+    const body = await response.json();
+
+    if (body.ok) {
+      localStorage.setItem('token', body.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+
+      const { uid, name } = body;
+
+      dispatch(login({ uid, name }));
+    } else {
+      Swal.fire('Error', body.msg, 'error');
     }
   };
 };
