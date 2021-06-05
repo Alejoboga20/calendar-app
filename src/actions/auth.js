5af0e2c32190fcf6cdb3d6be40752055c53fd78c
Alejo Boga <alejoboga20@gmail.com>
2021-06-05 11:43:57 -0300

    Dispatching login action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,5 @@
 import { fetchWithoutToken } from '../helpers/fetch';
+import { types } from '../types/types';
 
 export const startLogin = (email, password) => {
   return async (dispatch) => {
@@ -10,6 +11,18 @@ export const startLogin = (email, password) => {
 
     const body = await response.json();
 
-    console.log(body);
+    if (body.ok) {
+      localStorage.setItem('token', body.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+
+      const { uid, name } = body;
+
+      dispatch(login({ uid, name }));
+    }
   };
 };
+
+const login = (user) => ({
+  type: types.authLogin,
+  payload: user
+});
