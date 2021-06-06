5715e0d8c6df27b0cac23761ac7f6eca2a915155
Alejo Boga <alejoboga20@gmail.com>
2021-06-06 20:28:20 -0300

    Adding fetch with token

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -20,12 +20,21 @@ export const fetchWithToken = (endpoint, data, method = 'GET') => {
   const url = `${baseUrl}/${endpoint}`;
 
   if (method === 'GET') {
-    return fetch(url);
+    return (
+      fetch(url),
+      {
+        method,
+        headers: {
+          'x-token': localStorage.getItem('token') || ''
+        }
+      }
+    );
   } else {
     return fetch(url, {
       method,
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
+        'x-token': localStorage.getItem('token') || ''
       },
       body: JSON.stringify(data)
     });
