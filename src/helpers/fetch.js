166007cd53e9d9675c62aa5e4cd8fa6e337925b5
Alejo Boga <alejoboga20@gmail.com>
2021-06-06 20:26:29 -0300

    Adding fetch with token

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -15,3 +15,19 @@ export const fetchWithoutToken = (endpoint, data, method = 'GET') => {
     });
   }
 };
+
+export const fetchWithToken = (endpoint, data, method = 'GET') => {
+  const url = `${baseUrl}/${endpoint}`;
+
+  if (method === 'GET') {
+    return fetch(url);
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }
+};
