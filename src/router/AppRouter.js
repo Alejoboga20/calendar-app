8636868dc0e1414b569e28bf92de72ae8da6939f
Alejo Boga <alejoboga20@gmail.com>
2021-06-07 12:01:07 -0300

    Implementing protected routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,28 +1,41 @@
 import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 import { LoginScreen } from '../components/auth/LoginScreen';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   const dispatch = useDispatch();
+
+  const { checking, uid } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(startChecking());
   }, [dispatch]);
 
+  if (checking) {
+    return <h5>Wait...</h5>;
+  }
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path='/login' component={LoginScreen} />
-          <Route exact path='/' component={CalendarScreen} />
+          <PublicRoute
+            exact
+            path='/login'
+            component={LoginScreen}
+            isAuth={!!uid}
+          />
+          <PrivateRoute
+            exact
+            path='/'
+            component={CalendarScreen}
+            isAuth={!!uid}
+          />
 
           <Redirect to='/' />
         </Switch>
