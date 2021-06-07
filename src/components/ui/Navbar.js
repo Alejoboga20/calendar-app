1acf2662111791e1314688b97e803f0fa723f96f
Alejo Boga <alejoboga20@gmail.com>
2021-06-07 12:04:18 -0300

    Modifying navbar to receive name

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const Navbar = () => {
+export const Navbar = ({ name }) => {
   return (
     <div className='navbar navbar-dark bg-dark mb-4'>
-      <span className='navar-brand'>Alejo</span>
+      <span className='navar-brand'>{name}</span>
       <button className='btn btn-danger'>
         <span className='fas fa-sign-out-alt'> Exit</span>
       </button>
