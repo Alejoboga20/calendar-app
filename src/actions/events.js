76a50d6f4a01fd43a0c1f87e701e43f69edcc6c1
Alejo Boga <alejoboga20@gmail.com>
2021-06-08 09:49:02 -0300

    Creating action to save events in DB

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,11 +1,42 @@
+import { fetchWithToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
-export const eventAddNew = (event) => ({ type: types.eventAddNew, payload: event });
+export const eventStartAddNew = (event) => {
+  return async (dispatch, getState) => {
+    const { uid, name } = getState().auth;
 
-export const eventSetActive = (event) => ({ type: types.eventSetActive, payload: event });
+    try {
+      const response = await fetchWithToken('event', event, 'POST');
+      const body = await response.json();
 
-export const eventClearActiveEvent = () => ({ type: types.eventClearActiveEvent });
+      if (body.ok) {
+        event.id = body.event.id;
+        event.user = {
+          _id: uid,
+          name: name
+        };
+        dispatch(eventAddNew(event));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
-export const eventUpdated = (event) => ({ type: types.eventUpdated, payload: event });
+const eventAddNew = (event) => ({ type: types.eventAddNew, payload: event });
+
+export const eventSetActive = (event) => ({
+  type: types.eventSetActive,
+  payload: event
+});
+
+export const eventClearActiveEvent = () => ({
+  type: types.eventClearActiveEvent
+});
+
+export const eventUpdated = (event) => ({
+  type: types.eventUpdated,
+  payload: event
+});
 
 export const eventDeleted = () => ({ type: types.eventDeleted });
