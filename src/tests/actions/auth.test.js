b2495b07f0ca528fa1acab7cbea2db54af49301b
Alejo Boga <alejoboga20@gmail.com>
2021-06-09 11:40:42 -0300

    Tests for invalid login

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { startLogin } from '../../actions/auth';
 import { types } from '../../types/types';
+import Swal from 'sweetalert2';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -11,6 +12,10 @@ const initialState = {};
 let store = mockStore(initialState);
 
 Storage.prototype.setItem = jest.fn();
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
+}));
 
 describe('auth actions tests', () => {
   beforeEach(() => {
@@ -39,6 +44,18 @@ describe('auth actions tests', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith(
       'token-init-date',
       expect.any(Number)
+    );
+  });
+
+  test('should not login with incorrect password', async () => {
+    await store.dispatch(startLogin('alejotest@email.com', '123456789'));
+
+    const actions = store.getActions();
+    expect(actions).toEqual([]);
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'Password incorrect',
+      'error'
     );
   });
 });
