09e3f77d0e30a53219c3c174f8671cc15aeaeace
Alejo Boga <alejoboga20@gmail.com>
2021-06-10 14:34:31 -0300

    Testing startRegister

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,9 +1,10 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
-import { startLogin } from '../../actions/auth';
+import { startLogin, startRegister } from '../../actions/auth';
 import { types } from '../../types/types';
 import Swal from 'sweetalert2';
+import * as fetchModule from '../../helpers/fetch';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -56,6 +57,38 @@ describe('auth actions tests', () => {
       'Error',
       'Password incorrect',
       'error'
+    );
+  });
+
+  test('should startRegister', async () => {
+    fetchModule.fetchWithoutToken = jest.fn(() => ({
+      json() {
+        return {
+          ok: true,
+          uid: 'test',
+          name: 'testUser',
+          token: 'ABCD1234'
+        };
+      }
+    }));
+    await store.dispatch(
+      startRegister('testuser@email.com', '123456', 'testuser')
+    );
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: types.authLogin,
+      payload: {
+        uid: 'test',
+        name: 'testUser'
+      }
+    });
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('token', 'ABCD1234');
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'token-init-date',
+      expect.any(Number)
     );
   });
 });
