36e0d83b22e7d5f0abe6193eaf60164ae9f2069a
Alejo Boga <alejoboga20@gmail.com>
2021-06-11 11:55:10 -0300

    Adding test with mock function

--- src/tests/componets/ui/DeleteEventFab.test.js
+++ src/tests/componets/ui/DeleteEventFab.test.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { DeleteEventFab } from '../../../components/ui/DeleteEventFab';
+import { eventStartDelete } from '../../../actions/events';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -11,6 +12,10 @@ const mockStore = configureStore(middlewares);
 const initState = {};
 const store = mockStore(initState);
 store.dispatch = jest.fn();
+
+jest.mock('../../../actions/events', () => ({
+  eventStartDelete: jest.fn()
+}));
 
 const wrapper = mount(
   <Provider store={store}>
@@ -21,5 +26,10 @@ const wrapper = mount(
 describe('DeleteEventFab Tests', () => {
   test('should display properly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should call eventStartDelete', () => {
+    wrapper.find('button').prop('onClick')();
+    expect(eventStartDelete).toHaveBeenCalled();
   });
 });
