0d23781fc820fb68ec2699129b027d19fe82b14f
Alejo Boga <alejoboga20@gmail.com>
2021-06-11 11:49:36 -0300

    Adding test for deletebutton

---
+++ src/tests/componets/ui/DeleteEventFab.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { DeleteEventFab } from '../../../components/ui/DeleteEventFab';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {};
+const store = mockStore(initState);
+store.dispatch = jest.fn();
+
+const wrapper = mount(
+  <Provider store={store}>
+    <DeleteEventFab />
+  </Provider>
+);
+
+describe('DeleteEventFab Tests', () => {
+  test('should display properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
