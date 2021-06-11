0652f0d9566be0f92d3998574c8d6cc1a2a17c05
Alejo Boga <alejoboga20@gmail.com>
2021-06-11 15:26:49 -0300

    Adding tests for register

--- src/tests/componets/auth/LoginScreen.test.js
+++ src/tests/componets/auth/LoginScreen.test.js
@@ -4,7 +4,8 @@ import thunk from 'redux-thunk';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { LoginScreen } from '../../../components/auth/LoginScreen';
-import { startLogin } from '../../../actions/auth';
+import { startLogin, startRegister } from '../../../actions/auth';
+import Swal from 'sweetalert2';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -14,7 +15,12 @@ const store = mockStore(initState);
 store.dispatch = jest.fn();
 
 jest.mock('../../../actions/auth', () => ({
-  startLogin: jest.fn()
+  startLogin: jest.fn(),
+  startRegister: jest.fn()
+}));
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
 }));
 
 const wrapper = mount(
@@ -24,6 +30,8 @@ const wrapper = mount(
 );
 
 describe('LoginScreen Tests', () => {
+  beforeEach(() => jest.clearAllMocks());
+
   test('should display properly', () => {
     expect(wrapper).toMatchSnapshot();
   });
@@ -42,5 +50,63 @@ describe('LoginScreen Tests', () => {
     });
 
     expect(startLogin).toHaveBeenCalledWith('test@email.com', '123456');
+  });
+
+  test('should not register when passwords do not match', () => {
+    wrapper.find('input[name="RName"]').simulate('change', {
+      target: { name: 'RName', value: 'testUser' }
+    });
+
+    wrapper.find('input[name="REmail"]').simulate('change', {
+      target: { name: 'REmail', value: 'test@email.com' }
+    });
+
+    wrapper.find('input[name="RPassword"]').simulate('change', {
+      target: { name: 'RPassword', value: 'firstpass' }
+    });
+
+    wrapper.find('input[name="RConfirmPassword"]').simulate('change', {
+      target: { name: 'RConfirmPassword', value: 'secondpass' }
+    });
+
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault() {}
+    });
+
+    expect(startRegister).not.toHaveBeenCalled();
+    expect(Swal.fire).toHaveBeenCalledWith(
+      'Error',
+      'Passwords should match',
+      'error'
+    );
+  });
+
+  test('should start register when passwords match', () => {
+    wrapper.find('input[name="RName"]').simulate('change', {
+      target: { name: 'RName', value: 'testUser' }
+    });
+
+    wrapper.find('input[name="REmail"]').simulate('change', {
+      target: { name: 'REmail', value: 'test@email.com' }
+    });
+
+    wrapper.find('input[name="RPassword"]').simulate('change', {
+      target: { name: 'RPassword', value: 'firstpass' }
+    });
+
+    wrapper.find('input[name="RConfirmPassword"]').simulate('change', {
+      target: { name: 'RConfirmPassword', value: 'firstpass' }
+    });
+
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault() {}
+    });
+
+    expect(startRegister).toHaveBeenCalledWith(
+      'testUser',
+      'test@email.com',
+      'firstpass'
+    );
+    expect(Swal.fire).not.toHaveBeenCalled();
   });
 });
