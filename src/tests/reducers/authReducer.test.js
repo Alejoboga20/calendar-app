9484c37c593ca2f7d7ca4c763a26e20d3821fc62
Alejo Boga <alejoboga20@gmail.com>
2021-06-11 09:54:28 -0300

    Testing for login

--- src/tests/reducers/authReducer.test.js
+++ src/tests/reducers/authReducer.test.js
@@ -1,4 +1,5 @@
 import { authReducer } from '../../reducers/authReducer';
+import { types } from '../../types/types';
 
 const initialState = {
   checking: true,
@@ -6,9 +7,22 @@ const initialState = {
   name: null
 };
 
+const loginAction = {
+  type: types.authLogin,
+  payload: {
+    uid: '123ABC',
+    name: 'TestUser'
+  }
+};
+
 describe('', () => {
   test('should return initial state', () => {
     const state = authReducer(initialState, {});
     expect(state).toEqual(initialState);
+  });
+
+  test('should authenticate user', () => {
+    const state = authReducer(initialState, loginAction);
+    expect(state).toEqual({ checking: false, uid: '123ABC', name: 'TestUser' });
   });
 });
